96f1c1972cbcc55b8978ce0d0d3eef2ac25914f0
KineticLensman <john.kent.42@googlemail.com>
2019-10-28 18:01:07 +0000

    Add file via upload

---
+++ renaming-01.ps1
@@ -0,0 +1,43 @@
+﻿#Dir | Rename-Item -NewName { $_.Name -replace " ","_" }
+
+#Dir -filter *.jpg | %{Rename-Item $_ -NewName ("beach_trip_{0}.jpg" -f $nr++)}
+
+
+
+#F:\TMP\powershell-test-dummy-files
+
+#Dir | Rename-Item -NewName  { $_.Name  -replace "\(","_" }
+
+#Dir | Rename-Item -NewName  { $_.Name  -replace "\)","" }
+
+#Dir | Rename-Item -NewName  { "2019" + $_.Name  -replace "\)","" }
+
+#$nr = 1
+#Dir | Rename-Item -NewName  { $_.Name  + $nr++ }
+
+
+#Dir | Rename-Item -NewName  { $_.BaseName  -replace "4","92" }
+
+#Dir | $f = $_
+
+<#
+
+This is a multi-line comment.
+
+#>
+
+#$nj = 0
+#Dir | Rename-Item -NewName 
+#    {
+#        $_.BaseName  + $nj.ToString().PadLeft(2,'0') + ".txt"
+#    }
+ #   $nj = $nj + 1
+#
+
+$nr = 1
+ForEach ($file in Get-ChildItem)
+{
+    $file.name
+    Rename-Item $file -NewName ($file.BaseName  + $nr.ToString().PadLeft(2,'0') + ".txt")
+    $nr = $nr + 1
+}
